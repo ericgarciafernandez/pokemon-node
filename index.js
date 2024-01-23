f96af442ea12bf01f9ac1a8fd56d5d51845fa350
ericgarciafernandez <ericgarciafernandez18@gmail.com>
2024-01-23 23:21:32 +0100

    add whiteList

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const cors = require('cors');
 const axios = require('axios');
 const app = express();
 
+const whiteList = ['https://pokemon-react-nine-xi.vercel.app/', 'http://localhost:3000'];
 
-app.use(cors());
+app.use(cors({
+    origin: whiteList
+}));
 app.use(express.json());
 
 const getAllPokemons = (request) => {
